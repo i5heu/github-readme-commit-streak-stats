a324633291a9d92697d878fd6df5f5d460472a6f
i5heu <i5heu@i5h.eu>
2024-01-14 17:03:07 +0000

    graphql

--- cmd/runServer/main.go
+++ cmd/runServer/main.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"fmt"
+	"log"
 	"net/http"
+	"os"
 
 	"github.com/i5heu/github-readme-commit-streak-stats/internal/generate"
 	serveui "github.com/i5heu/github-readme-commit-streak-stats/internal/serveUi"
@@ -12,5 +15,13 @@ func main() {
 	mux := http.NewServeMux()
 	mux.Handle("/", handler)
 	mux.HandleFunc("/generate", generate.GenerateHandler)
+
+	// checks
+
+	if os.Getenv("GITHUB_TOKEN") == "" {
+		log.Fatal("GITHUB_TOKEN environment variable not set")
+	}
+
+	fmt.Println("Starting server on port http://localhost:8080/")
 	http.ListenAndServe(":8080", mux)
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,17 @@
 module github.com/i5heu/github-readme-commit-streak-stats
 
 go 1.21.5
+
+require github.com/shurcooL/githubv4 v0.0.0-20231126234147-1cffa1f02456
+
+require (
+	github.com/golang/protobuf v1.5.3 // indirect
+	golang.org/x/net v0.20.0 // indirect
+	google.golang.org/appengine v1.6.7 // indirect
+	google.golang.org/protobuf v1.31.0 // indirect
+)
+
+require (
+	github.com/shurcooL/graphql v0.0.0-20230722043721-ed46e5a46466 // indirect
+	golang.org/x/oauth2 v0.16.0
+)

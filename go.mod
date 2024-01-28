285ebae4860ea9b83b820da135495a8989018e7d
i5heu <i5heu@i5h.eu>
2024-01-28 19:09:49 +0000

    add image gen

--- go.mod
+++ go.mod
@@ -5,13 +5,16 @@ go 1.21.5
 require github.com/shurcooL/githubv4 v0.0.0-20231126234147-1cffa1f02456
 
 require (
+	github.com/golang/freetype v0.0.0-20170609003504-e2365dfdc4a0 // indirect
 	github.com/golang/protobuf v1.5.3 // indirect
+	golang.org/x/image v0.15.0 // indirect
 	golang.org/x/net v0.20.0 // indirect
 	google.golang.org/appengine v1.6.7 // indirect
 	google.golang.org/protobuf v1.31.0 // indirect
 )
 
 require (
+	github.com/fogleman/gg v1.3.0
 	github.com/shurcooL/graphql v0.0.0-20230722043721-ed46e5a46466 // indirect
 	golang.org/x/oauth2 v0.16.0
 )
--- internal/generate/generate.go
+++ internal/generate/generate.go
@@ -3,16 +3,14 @@ package generate
 import (
 	"fmt"
 	"net/http"
+	"strconv"
 
+	"github.com/fogleman/gg"
 	"github.com/i5heu/github-readme-commit-streak-stats/internal/getData"
 )
 
 func GenerateHandler(w http.ResponseWriter, r *http.Request) {
 	githubUser := r.URL.Query().Get("githubUser")
-	mode := r.URL.Query().Get("mode")
-	strictness := r.URL.Query().Get("strictness")
-
-	fmt.Println(githubUser, mode, strictness)
 
 	cdc, err := getData.GetCommitDates(githubUser)
 	if err != nil {
@@ -20,7 +18,28 @@ func GenerateHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	totalCommits := 0
 	for _, cd := range cdc.CommitData {
-		w.Write([]byte(fmt.Sprintf("%d-%d-%d %d\n", cd.Year, cd.Month, cd.Day, cd.Count)))
+		totalCommits += cd.Count
+	}
+
+	firstCommit := cdc.CommitData[len(cdc.CommitData)-1]
+	firstCommitDate := fmt.Sprintf("%d-%02d-%02d", firstCommit.Year, firstCommit.Month, firstCommit.Day)
+
+	const W = 400
+	const H = 200
+	dc := gg.NewContext(W, H)
+
+	dc.SetRGB(1, 1, 1)
+	dc.Clear()
+
+	dc.SetRGB(0, 0, 0)
+	dc.DrawStringAnchored("Total commits from "+firstCommitDate+" - Present: "+strconv.Itoa(totalCommits), W/2, H/2, 0.5, 0.5)
+	dc.Stroke()
+
+	err = dc.EncodePNG(w)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
 	}
 }
